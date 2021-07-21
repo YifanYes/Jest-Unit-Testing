8f4639a37197554087b80de13cff02ebe92958bc
Yifan <yifanyemontpe@gmail.com>
2021-07-21 07:39:20 +0200

    Added constants and modified functions accordingly

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-// this is my function that sums two numbers
+
 const sum = (a,b) => {
     return a + b
 }
-
-// just a console log for ourselves.
 console.log(sum(7,3))
 
-// export the function to be used on other files 
-// (similar to the keyword `export` when using webpack)
-
-const fromDollarToYen = (valueInDollar) => {
-    let valueInYen = valueInDollar * 1.2 / 127.9 ;
-    return valueInYen;
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
 }
 
-const fromEuroToDollar = (valueInEuro) => {
-    let valueInDollar = valueInEuro * 1.2;
-    return valueInDollar;
+const fromEuroToDollar = (euroAmmount) => {
+    let dollarResult = euroAmmount * oneEuroIs.USD;
+    return dollarResult;
 }
 
-const fromYanToPound = (valueInYan) => {
-    let valueInPound = valueInYan * 127.9 / 0.8;
-    return valueInPound;
+const fromDollartoYen = (dollarAmmount) => {
+    let yenResult = (dollarAmmount / oneEuroIs.USD) * oneEuroIs.JPY;
+    return yenResult;
+}
+
+const fromYentoPound = (yenAmmount) => {
+    let poundResult = (yenAmmount / oneEuroIs.JPY) * oneEuroIs.GBP;
+    return poundResult;
 }
 
 module.exports = { sum , fromEuroToDollar, fromDollarToYen, fromYanToPound };
